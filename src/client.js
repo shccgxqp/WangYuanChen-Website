735a56200cea216e1ccfdc843e71be64a4c627e0
shccgxqp <shccgxqp@gmail.com>
2024-01-08 20:15:38 +0800

    test

--- src/client.js
+++ src/client.js
@@ -2,11 +2,11 @@ import sanityClient from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
 export const client = sanityClient({
-  projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
+  projectId: 'cw2mjpry',
   dataset: 'production',
   apiVersion: '2022-02-01',
   useCdn: true,
-  token: process.env.REACT_APP_SANITY_TOKEN,
+  token: 'skbjAxRYpRSfJ5MO9CDaL490BCUUCKbbN68Z6ibva7WOm27YYD7RbFli7i5PGPNGxEy9FZnfuIPKuPRzMR4HHbFqIY6TA4sPyJZX3aMIweIxqj2jP5a5QjoPW8phYpnP1VO6lyGzVjcbqjysyb82aNowrlohSMYss1obDkhWoRcAbvvnb3kS',
 });
 
 const builder = imageUrlBuilder(client);
